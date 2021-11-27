fe7009df6a2a43aec4685c4506d81e06809be543
dspivak152 <dspivak152@gmail.com>
2021-11-27 10:19:20 +0200

    working on login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    InventoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+const AUTH_API = 'http://sinfori.com:3080/login';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient, public snackBar: MatSnackBar) { }
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(AUTH_API, { username: "admin", password: "mini" }, httpOptions).pipe(
+      catchError(this.handleError<any>('Unable to create new room')));;;
+  }
+
+  handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(error);
+      this.snackBar.open(error.statusText, '', { duration: 2000 });
+      return of(result as T);
+    };
+  }
+}

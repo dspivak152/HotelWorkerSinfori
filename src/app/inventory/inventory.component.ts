f67e63916bd4970f441f5485e1a349c036dde1a1
dspivak152 <dspivak152@gmail.com>
2021-11-29 20:49:41 +0200

    bad logic for missing rooms per item but it works

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -11,7 +11,9 @@ export class InventoryComponent implements OnInit {
   inventoryTabsConsts = InventoryTabsConsts;
   currentTab: string = this.inventoryTabsConsts.ROOMS_OVERVIEW;
   hotelDevices: any[] = [];
-  devicesResult: any[] = [];
+  inventoryData: any[] = [];
+  missingItems: any[] = [];
+  missingItemsWithRooms: any[] = [];
 
   constructor(private inventoryService: InventoryService) { }
 
@@ -20,26 +22,55 @@ export class InventoryComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.showInventoryForHotel()
+    this.getInventoryForHotel()
   }
 
-  showInventoryForHotel() {
+  getInventoryForHotel() {
     this.inventoryService.getRoomsOverviewData().subscribe(result => {
       if (result != undefined) {
-        this.hotelDevices = result;
+        this.hotelDevices = JSON.parse(JSON.stringify(result));
+        this.inventoryData = JSON.parse(JSON.stringify(result));
         this.filterHotelDevices();
+        this.setDataForInventoryView();
       }
     })
   }
 
   filterHotelDevices() {
     this.hotelDevices.forEach(device => {
-      let newArray = device.products.filter(function (x: any) {
+      let missingProducts = device.products.filter(function (x: any) {
         return x.slotBoundToProduct && x.slotIsEmpty
       })
-      device.missingProducts = newArray.map((x: any) => x.product.name)
-      device = newArray;
+      device.missingProducts = missingProducts.map((x: any) => x.product.name);
     })
+  }
+
+  setDataForInventoryView() {
+    this.inventoryData.forEach(device => {
+      let missingProducts = device.products.filter(function (x: any) {
+        return x.slotBoundToProduct && x.slotIsEmpty
+      })
+      missingProducts.forEach((product: any) => {
+        if (this.missingItems.find(x => x.id == product.product.id) == undefined) {
+          this.missingItems.push({ id: product.product.id, name: product.product.name })
+        }
+      });
+    });
+    this.inventoryData.forEach(room => {
+      room.products.forEach((product: any) => {
+        let foundProduct = this.missingItems.find(item => item.id === product.product.id);
+        if (foundProduct && product.slotBoundToProduct && product.slotIsEmpty) {
+          let foundExistingMissingItem = this.missingItemsWithRooms.find(x => x.productName == foundProduct.name);
+          if (foundExistingMissingItem) {
+            foundExistingMissingItem.rooms.indexOf(room.roomNumber) === -1 ? foundExistingMissingItem.rooms.push(room.roomNumber) : console.log("This item already exists");
+          } else {
+            this.missingItemsWithRooms.push({ productName: foundProduct.name, rooms: [room.roomNumber] })
+          }
+        }
+      });
+    })
+    console.log(this.missingItemsWithRooms)
+
   }
 
 }
